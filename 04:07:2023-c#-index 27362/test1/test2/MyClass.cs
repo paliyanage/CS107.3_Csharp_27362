bec8ec39e05bfa96c033cd6ddbcf94d571e7eb83
paliyanage <136835688+paliyanage@users.noreply.github.com>
2023-07-04 19:28:06 +0530

    Add files via upload

---
+++ 04:07:2023-c#-index 27362/test1/test2/MyClass.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace test2
+{
+	public class ArrayClass
+	{
+		public ArrayClass()
+		{
+            public int[] arrayMethod(int size)
+            {
+                int[] array = new int[size];
+                for (int i = 0; i < array.length; i += 2)
+                {
+                    console.WriteLine("Enter a value:");
+                    int value = Convert.ToInt32(Console.ReadLine());
+
+                    array[i] = value;
+                    array[] = (int)value
+
+                }
+            }
+        }
+	}
+}
+
