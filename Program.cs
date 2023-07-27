8f7ea971c72a8f9698bc06f554b031dc3b998c4d
paliyanage <136835688+paliyanage@users.noreply.github.com>
2023-07-27 14:55:30 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__Lab_quetion_05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.Write("Enter a number: ");
+                int number = Convert.ToInt32(Console.ReadLine());
+
+                if (number == 0)
+                {
+                    Console.WriteLine("The number is zero.");
+                }
+                else if (number % 2 == 0)
+                {
+                    Console.WriteLine("The number is even.");
+                }
+                else
+                {
+                    Console.WriteLine("The number is odd.");
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
