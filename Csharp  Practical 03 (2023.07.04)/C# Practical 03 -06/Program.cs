7bc9e5024c85c1156642c68e8476dd5a0d5388da
paliyanage <136835688+paliyanage@users.noreply.github.com>
2023-07-27 15:23:05 +0530

    Add files via upload

---
+++ Csharp  Practical 03 (2023.07.04)/C# Practical 03 -06/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__Practical_03__06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the size of the Array:");
+            int size = Convert.ToInt32(Console.ReadLine());
+
+            ArrayOperations arrayOps = new ArrayOperations();
+            arrayOps.FillArray(size);
+            Console.ReadLine();
+        }
+    }
+}
