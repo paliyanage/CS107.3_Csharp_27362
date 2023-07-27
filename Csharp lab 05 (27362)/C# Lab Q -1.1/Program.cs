0ddb45a40f498e3ad728fd9b3156a45f75d7fce5
paliyanage <136835688+paliyanage@users.noreply.github.com>
2023-07-27 15:15:42 +0530

    Add files via upload

---
+++ Csharp lab 05 (27362)/C# Lab Q -1.1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__Lab_Q__1._1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Convertvalues converter = new Convertvalues();
+            Converter.kilometerTometer();
+        }
+    }
+}
